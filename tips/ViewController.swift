37c898b6dd48f76e799d391e2dc1450562172408
gyqi <gyqi28@gmail.com>
2016-01-17 20:35:37 -0800

    Commit tips project

---
+++ tips/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Grace Qi on 1/14/16.
+//  Copyright © 2016 Grace Qi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        let newColor = UIColor(red: CGFloat(198)/255,
+            green: CGFloat(225)/255,
+            blue: CGFloat(255)/255,
+            alpha: 1.0)
+        self.view.backgroundColor = newColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let tipPercentages = [0.18, 0.2, 0.22]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
